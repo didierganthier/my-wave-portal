610a7eb6984de6e5bde7076f14782bfc70263547
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-07-09 23:01:48 -0400

    contract modified

---
+++ .history/contracts/WavePortal_20220709224842.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+
+    uint256 totalWaves;
+    constructor(){
+        console.log("Hello, I am a contract and I am smart");
+    }
+}
--- .history/contracts/WavePortal_20220709225021.sol
+++ .history/contracts/WavePortal_20220709225021.sol
@@ -15,9 +15,4 @@ contract WavePortal {
         totalWaves += 1;
         console.log("%s has waved!", msg.sender);
     }
-
-    function getTotalWaves() public view returns (uint256) {
-        console.log("We have %d total waves", totalWaves);
-        return totalWaves;
-    }
 }
